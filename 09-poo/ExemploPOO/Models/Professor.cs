3b07d9fbbe395c84ab10ef5e9d28d598a9d26e3b
LXSCA <lucas.edr27@gmail.com>
2024-05-11 21:20:27 -0300

    feat: aula de POO

---
+++ 09-poo/ExemploPOO/Models/Professor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class Professor : Pessoa
+    {
+        public decimal Salario { get; set; }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, me chamo {Nome}, tenho {Idade} anos, sou um professor e ganho {Salario}");
+        }
+    }
+}
---
+++ 09-poo/ExemploPOO/Program.cs
@@ -0,0 +1,28 @@
+﻿using ExemploPOO.Models;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Aluno a1 = new Aluno();
+        a1.Nome = "Lucas";
+        a1.Idade = 18;
+        a1.Nota = 10;
+
+
+        Professor p1 = new Professor() {
+            Nome = "Cláudio",
+            Idade = 44,
+            Salario = 1426.14M
+        };
+
+        Diretor d1 = new Diretor() {
+            Nome = "Márcio",
+            Idade = 63,
+            Salario = 5156.15M
+        };
+
+        a1.Apresentar();
+        p1.Apresentar();
+        d1.Apresentar();
+    }
+}
